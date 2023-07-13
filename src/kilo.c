8b0a0ec06fab10506a330ccd2afb1db5b7ed0177
shaanNagra <shaan.n@outlook.co.nz>
2023-07-14 11:56:46 +1200

    output byte by byte read in

--- src/kilo.c
+++ src/kilo.c
@@ -1,7 +1,10 @@
+#include <ctype.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
-// #include <stdio.h>
+// #include <stdio.h> 
+
 struct termios orig_termios;
 
 void disableRawMode(){	
@@ -30,7 +33,14 @@ int main(){
   enableRawMode();
   
   char c;
-  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q'){
+    if(iscntrl(c)) {
+      printf("%d\n", c);
+    } else {
+      printf("%d ('%c')\n", c, c);
+    }      
+  }
+  
   return 0;
 
 }

6a98468f06e9e7cb3227a1d1a3ee52e8caecb8f8
shaanNagra <shaan.n@outlook.co.nz>
2023-07-14 12:28:01 +1200

    disables misc flags

--- src/kilo.c
+++ src/kilo.c
@@ -24,16 +24,23 @@ void enableRawMode() {
   //FLAGSET = FLAGS AND NOT (X Y Z)
   //ICRNL FLAG: carriage return new line flag ctrl-m now 13 
   //IXON FLAG: suspend software control signals ctrl-s/-q
-  raw.c_iflag &= ~(ICRNL | IXON);
+  raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
   //OPOST FLAG: output processing 
   // e.g. by default it a `\n` new line is processed as a `\r\n` so the new line
   //      begins at start of next line 
   raw.c_oflag &= ~(OPOST);
+  raw.c_cflag &= ~(CS8);
   //ECHO FLAG: print input to terminal
   //ICANNON FLAG: read line by line not byte by byte
   //IEXTEN FLAG: disable ctrl-v 
   //ISIS FLAG: suspend ctrl-c/-z sending signal inputs
   raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+  //Miscellaneous flags
+  //  wont have any obserbale effect as they are already or turned off or dont 
+  //  apply to modern term emnulators. but used to be considered to be apart of
+  //  enabling raw mode.
+  //BRKINT, INPCK, ISTRIP, and CS8 all come from <termios.h>.
+
 
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }

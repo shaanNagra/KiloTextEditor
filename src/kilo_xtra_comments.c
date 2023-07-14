fe2759987e85cda63b4b33af842fd242ef33802b
shaanNagra <shaan.n@outlook.co.nz>
2023-07-14 15:04:09 +1200

    added comments to functions, plus created new file with comments of how some c code works

--- src/kilo_xtra_comments.c
+++ src/kilo_xtra_comments.c
@@ -30,7 +30,10 @@ struct termios orig_termios;
  * read from variable errno, then exits program with non-zero status. 
  */
 void die(const char *s){
+  //PERROR: first prints string passed to it. then looks at errno var and prints
+  //        a descriptive err msg. most c libs funcs that fail set errno
   perror(s);
+  //EXIT:   exit program with status 1 (indicated failure)
   exit(1);
 }
 
@@ -40,7 +43,8 @@ void die(const char *s){
  * Description: Sets terminal back to normal mode. Undoes attributes changed by
  * enableRawMode.
  */
-void disableRawMode(){
+void disableRawMode(){	
+  //TCSAFLUSH:  will discard any unread input before applying changes.
   if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1) 
     die("tcsetattr");
 }
@@ -55,34 +59,33 @@ void disableRawMode(){
 void enableRawMode() {
  
   if (tcgetattr(STDIN_FILENO, &orig_termios) == -1)
-    die("tcgetattr");
-
-  //atexit: comes from <stdlib.h>. Registers function to be called 
-  //automatically when the program exits.
+    die("tcgetattr");  
+  //ATEXIT: comes from <stdlib.h>.
+  //        use it to register our function to be called automatically when the
+  //        program exits.
   atexit(disableRawMode);
 
   struct termios raw = orig_termios;
-  /*
-   * Note: the flags that were disabled and what they did.
-   * 
-   * ICRNL: ctrl-m and enter modified to not clash with CR. 
-   * IXON:  software control signals ctrl-s/-q.
-   * OPOST: ouput processing. e.g., \n results in \r\n.
-   * ECHO:  echo input to terminal.    
-   * ICANON:read input line by line. 
-   * IEXTEN:ctrl-v.
-   * ISIG:  signal inputs from application. ctrl-c/-z.
-   *  
-   * Misc flags:  BRKINT, INPCK, ISTRIP, and CS8.
-   */
-
+  //ASSIGNMENT EXPRESSION: flags = set flags and not (bit x,y,z)
+  //ICRNL FLAG:   carriage return new line flag ctrl-m now 13 
+  //IXON FLAG:    suspend software control signals ctrl-s/-q
   raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+  //OPOST FLAG:   output processing   e.g. by default it a `\n` new line is
+  //              processed as a `\r\n` so the new line begins at start. 
   raw.c_oflag &= ~(OPOST);
   raw.c_cflag &= ~(CS8);
+  //ECHO FLAG:    print input to terminal
+  //ICANON FLAG:  read line by line not byte by byte
+  //IEXTEN FLAG:  disable ctrl-v 
+  //ISIS FLAG:    suspend ctrl-c/-z sending signal inputs
   raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+  //Notes on miscellaneous flags
+  //  wont have any obserbale effect as they are already or turned off or dont 
+  //  apply to modern term emnulators. but used to be considered to be apart of
+  //  enabling raw mode.
+  //BRKINT, INPCK, ISTRIP, and CS8 all come from <termios.h>.
 
-
-  //Timeout read()
+  //TIMEOUT READ()
   raw.c_cc[VMIN] = 0;
   raw.c_cc[VTIME] = 1;
 
